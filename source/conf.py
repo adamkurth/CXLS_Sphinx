5947af4e19c2ee2d6fc9938d89df3a58f7de67fb
Adam Kurth <amkurth@asu.edu>
2023-11-29 10:10:45 -0700

    progress 11/29

--- source/conf.py
+++ source/conf.py
@@ -24,5 +24,6 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'furo'
-# html_static_path = ['_static']
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+ 
