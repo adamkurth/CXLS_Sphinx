d1de13983b9be0c5150df75351407273d39ac1be
Adam Kurth <amkurth@asu.edu>
2023-11-30 11:20:40 -0700

    progress 11/30

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,7 +9,7 @@
 project = 'CXLS Sphinx'
 copyright = '2023, Sabine Botha, Adam Kurth, and more'
 author = 'Sabine Botha, Adam Kurth, and more'
-release = 'N'
+# release = 'N'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -24,11 +24,20 @@ extensions = [
 
 templates_path = ['_templates']
 exclude_patterns = []
+pygments_style = 'sphinx'
 
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'sphinxawesome_theme'
 html_static_path = ['_static']
+
+# themes installed with pip 
+# 'sphinx_rtd_theme'
+# 'insegel'
+# 'sphinx_material'
+# 'sphinx_book_theme'
+# 'sphinx_py3doc_enhanced_theme'
+# 'sphinxawesome_theme'
